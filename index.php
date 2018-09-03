ee1fda8a6cc1c5cda22ab488f5d027806c7797b2
Perrine <perrine.salce@hotmail.com>
2018-09-03 16:57:35 +0200

    Disparition formulaires et début ajout et retrait de points

--- index.php
+++ index.php
@@ -2,44 +2,45 @@
 
 <!-- Premier compteur -------------------------------------------------------->
 
-
 <form class="compteur1" action="index.php" method="post">
-    <input type="number" name="compt1" value="8000" disabled>
-    <button type="button" name="more1" id="more1">+</button>
-    <button type="button" name="less1" id="less1">-</button>
+        <input type="number" name="compt1" value="8000" disabled id="compt1">
+        <button type="button" name="more1" id="more1">+</button>
+        <button type="button" name="less1" id="less1">-</button>
+
+        <div id="add1">
+            <label for="toAdd1">Combien de points avez vous gagné ?</label>
+            <input type="number" name="toAdd1" id="toAdd1">
+            <input type="button" name="addOne" value="Ajouter" id="addOne">
+            <input type="button" name="annule1" value="Annuler" id="annule1">
+        </div>
+
+        <div id="lost1">
+            <label for="toRemove1">Combien de points avez vous perdu ?</label>
+            <input type="number" name="toRemove1" id="toRemove1">
+            <input type="button" name="removeOne" value="Retirer" id="removeOne">
+            <input type="button" name="annule2" value="Annuler" id="annule2">
+        </div>
 </form>
 
-
-<form id="add1" action="index.php" method="post">
-    <label for="addOne">Combien de points avez vous gagné ?</label>
-    <input type="number" name="addOne">
-    <input type="submit" name="addIt1" value="Ajouter">
-</form>
-
-<form id="lost1" action="index.php" method="post">
-    <label for="lostOne">Combien de points avez vous perdu ?</label>
-    <input type="number" name="lostOne">
-    <input type="submit" name="lostIt1" value="Retirer">
-</form>
-
-
-<!-- Deuxième compteur -------------------------------------------------------->
+<!-- Deuxième compteur ------------------------------------------------------>
 <form class="compteur2" action="index.php" method="post">
-    <input type="number" name="compt2" value="8000" disabled>
-    <button type="button" name="more2" id="more2">+</button>
-    <button type="button" name="less2" id="less2">-</button>
-</form>
+        <input type="number" name="compt2" value="8000" disabled id="compt2">
+        <button type="button" name="more2" id="more2">+</button>
+        <button type="button" name="less2" id="less2">-</button>
 
-<form id="add2" action="index.php" method="post">
-    <label for="addTwo">Combien de points avez vous gagné ?</label>
-    <input type="number" name="addTwo">
-    <input type="submit" name="addIt2" value="Ajouter">
-</form>
+    <div id="add2">
+        <label for="toAdd2">Combien de points avez vous gagné ?</label>
+        <input type="number" name="toAdd2" id="toAdd2">
+        <input type="button" name="addTwo" value="Ajouter" id="addTwo">
+        <input type="button" name="annule3" value="Annuler" id="annule3">
+    </div>
 
-<form id="lost2" action="index.php" method="post">
-    <label for="lostTwo">Combien de points avez vous perdu ?</label>
-    <input type="number" name="lostTwo">
-    <input type="submit" name="lostIt2" value="Retirer">
+    <div id="lost2">
+        <label for="toRemove2">Combien de points avez vous perdu ?</label>
+        <input type="number" name="toRemove2" id="toRemove2">
+        <input type="button" name="removeTwo" value="Retirer" id="removeTwo">
+        <input type="button" name="annule4" value="Annuler" id="annule4">
+    </div>
 </form>
 
 <?php require('footer.php') ?>
--- js/script.js
+++ js/script.js
@@ -7,6 +7,11 @@ $(document).ready(function() {
     var more2 = $('#more2');
     var less2 = $('#less2');
 
+    var annule1 = $('#annule1');
+    var annule2 = $('#annule2');
+    var annule3 = $('#annule3');
+    var annule4 = $('#annule4');
+
 /*Variables formulaires------------------------------------------------------*/
     var add1 = $('#add1');
     var add2 = $('#add2');
@@ -16,17 +21,87 @@ $(document).ready(function() {
 /*Fonctions apparition des formulaires --------------------------------------*/
     $(more1).click(function () {
             $(add1).css("display", "block")
+
+            if ($(lost1).css("display", "block")) {
+                $(lost1).css("display", "none")
+            }
         })
 
     $(less1).click(function () {
             $(lost1).css("display", "block")
+
+            if ($(add1).css("display", "block")) {
+                $(add1).css("display", "none")
+            }
         })
 
     $(more2).click(function () {
             $(add2).css("display", "block")
+
+            if ($(lost2).css("display", "block")) {
+                $(lost2).css("display", "none")
+            }
         })
 
     $(less2).click(function () {
             $(lost2).css("display", "block")
+
+            if ($(add2).css("display", "block")) {
+                $(add2).css("display", "none")
+            }
         })
+
+/*Fonctions disparition des formulaires -------------------------------------*/
+    $(annule1).click(function () {
+            $(add1).css("display", "none")
+        })
+
+    $(annule2).click(function () {
+            $(lost1).css("display", "none")
+        })
+
+    $(annule3).click(function () {
+            $(add2).css("display", "none")
+        })
+
+    $(annule4).click(function () {
+            $(lost2).css("display", "none")
+        })
+
+/*Ajouts et retrait de points------------------------------------------------*/
+
+/*Variables des champs compteurs---------------------------------------------*/
+    var compt1 = $('#compt1').val();
+    var compt2 = $('#compt2').val();
+
+/*Variables boutons validation de l'ajout et du retrait----------------------*/
+    var addOne = $('#addOne');
+    var removeOne = $('#removeOne');
+    var addTwo = $('#addTwo');
+    var removeTwo = $('#removeTwo');
+
+/*Variables champs de valeur à ajouter ou à retirer--------------------------*/
+    var toAdd1 = $('#toAdd1').val();
+    var toRemove1 = $('#toRemove1').val();
+    var toAdd2 = $('#toAdd2').val();
+    var toRemove2 = $('#toRemove2').val();
+
+/*Fonctions d'ajout de points------------------------------------------------*/
+    $(addOne).click(function() {
+        compt1 += toAdd1;
     })
+
+    $(addTwo).click(function() {
+        compt2 += toAdd2;
+    })
+
+/*Fonctions de retraits de points--------------------------------------------*/
+    $(removeOne).click(function() {
+        compt1 -= toRemove1;
+    })
+
+    $(removeTwo).click(function() {
+        compt2 -= toRemove2;
+    })
+
+})
